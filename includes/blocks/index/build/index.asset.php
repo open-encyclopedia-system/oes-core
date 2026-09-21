184e85dd47665e70fe3e5f8d8eb5d86278bd8841
Maren Strobl <maren.strobl@fu-berlin.de>
2026-09-21 10:47:18 +0200

    prepare 3.0.0, new parameter for block "Index"

--- includes/blocks/index/build/index.asset.php
+++ includes/blocks/index/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '4f47e593cd59f0b7cf01');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'afb7dc05c7f5bb2d2a00');
